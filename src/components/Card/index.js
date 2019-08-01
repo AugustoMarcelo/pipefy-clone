3bfa056e4df4eaf3863f8f6862d6f91dcca5e09f
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-01 19:19:56 -0300

    Adding libs to handle drag functionality

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { useDrag } from 'react-dnd';
 
 import { Container, Label } from './styles';
 
-export default function Card() {
+export default function Card({ data }) {
+  const [{ isDragging }, dragRef] = useDrag({
+    item: { type: 'CARD' },
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
   return (
-    <Container>
+    <Container ref={dragRef} isDragging={isDragging}>
       <header>
-        <Label color="#7159c1" />
+        {data.labels.map(label => <Label key={label} color={label} />)}
       </header>
-      <p>Fazer migração completa de servidor</p>
-      <img src="https://api.adorable.io/avatars/285/abott@adorable.png" alt=""/>
+      <p>{data.content}</p>
+      {data.user && <img src={data.user} alt=""/> }
     </Container>
   );
 }
